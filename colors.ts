138245601a29ce19f07dd3b92180828e704bd749
Brenna Martenson <martensonbj@gmail.com>
2019-07-15 17:23:30 -0500

    Phase 1 of styling components

---
+++ colors.ts
@@ -0,0 +1,47 @@
+export const teals = [
+  '#06FCDE',
+  '#05E2D1',
+  '#05D3CD',
+  '#04BABA',
+  '#037177',
+  '#013435',
+]
+
+export const blues = [
+  '#03DEFC',
+  '#05B6D3',
+  '#059DC6',
+  '#027FA5',
+  '#035E7A',
+  '#012F3F',
+]
+
+export const yellows = [
+  '#FCDD03',
+  '#CE9905',
+  '#C17C02',
+  '#9E6202',
+  '#7C4A01',
+  '#472C01',
+]
+
+export const reds = [
+  '#FC1557',
+  '#E20544',
+  '#BC0240',
+  '#99042E',
+  '#84022A',
+  '#420118',
+]
+
+export const greys = [
+  '#F7F5F3',
+  '#DDDDDD',
+  '#C3C3BD',
+  '#7F7E7B',
+  '#3D3937',
+  '#282625',
+]
+
+const colors = { teals, blues, yellows, reds, greys }
+export default colors
